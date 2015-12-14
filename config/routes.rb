ab2185907abf7ee7aca6f07d89139e7a41595fb7
Damian Trojnar <trojnar.damian@gmail.com>
2015-12-14 09:01:08 +0100

    [API#0] Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   resources :payments, only: [:index, :new, :create]
 
   root to: "payments#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index]
+      resources :payments, only: [:index, :create]
+    end
+  end
 end
