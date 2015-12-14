84470223bed9b409a5e533c4e3ad1bb3745cd7b8
Damian Trojnar <trojnar.damian@gmail.com>
2015-12-14 09:02:35 +0100

    [API#3] Update AngularJS Payment & User service

--- app/assets/javascripts/services/payment.js
+++ app/assets/javascripts/services/payment.js
@@ -1,26 +1,7 @@
-angular.module('szama').service('Payment', function(){
-
-  this.index = function() {
-    return [
-      {
-        title: "Pizza",
-        creator_id: 1,
-        balance_changes: [
-          { user_id: 1, change: -10.0 },
-          { user_id: 2, change: -15.0 },
-          { user_id: 1, change: 25.0 },
-        ],
-      },
-      {
-        title: "Bagiety",
-        creator_id: 2,
-        balance_changes: [
-          { user_id: 1, change: -12.0 },
-          { user_id: 2, change: -14.0 },
-          { user_id: 2, change: 26.0 },
-        ]
-      },
-    ];
-  };
-
+angular.module('szama').service('Payment', function($http){
+  // https://docs.angularjs.org/api/ng/service/$http
+  var api_endpoint = "/api/v1/payments";
+  this.index = function(){ return $http.get(api_endpoint); }
+  this.create = function(params){ return $http.post(api_endpoint, params); }
+  return this;
 });
--- app/assets/javascripts/services/user.js
+++ app/assets/javascripts/services/user.js
@@ -1,10 +1,8 @@
-angular.module('szama').service('User', function(){
-
-  this.index = function() {
-    return {
-      1: { name: "Jon", balance: 3.0 },
-      2: { name: "Hodor", balance: -3.0 },
-    };
-  };
-
+// This should be maybe called userHttp now, but let's stay with what we got for now
+angular.module('szama').service('User', function($http){
+  // $http is angular service for doing http request
+  // https://docs.angularjs.org/api/ng/service/$http
+  var api_endpoint = "/api/v1/users";
+  this.index = function(){ return $http.get(api_endpoint); }
+  return this;
 });
